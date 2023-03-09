fdcff7d32ab7944ac244e03793966ee5004b002e
Destiny Boone <destaj.db@gmail.com>
2023-03-09 12:05:14 -0500

    working on the Cart component

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -16,7 +16,7 @@ const MealItem = props => {
                 </div>
             </div>
             <div>
-                <MealItemForm />
+                <MealItemForm id={props.id}/>
             </div>
         </li>
     );
