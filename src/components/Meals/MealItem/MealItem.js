f811a5a30f44adc577355b5116d98e3c39c824a0
Destiny Boone <destaj.db@gmail.com>
2023-03-09 11:07:49 -0500

    adding a form

--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,4 +1,5 @@
 import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
 
 const MealItem = props => {
     const price = `$${props.price.toFixed(2)}`;
@@ -15,7 +16,7 @@ const MealItem = props => {
                 </div>
             </div>
             <div>
-
+                <MealItemForm />
             </div>
         </li>
     );
---
+++ src/components/UI/Input.js
@@ -0,0 +1,12 @@
+import classes from './Input.module.css';
+
+const Input = props => {
+    return(
+        <div className={classes.input}>
+            <label htmlFor={props.input.id}>{props.label}</label>
+            <input {...props.input}/> {/* spread operator ensures all key-value pairs in this input object which we receive on props.input are added as props to input */}
+        </div>
+    );
+};
+
+export default Input;
