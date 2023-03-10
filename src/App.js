206c1c65433d4cb95dfc46c7eddffc381603f6c3
Destiny Boone <destaj.db@gmail.com>
2023-03-10 16:39:22 -0500

    Revert "adding a cart context"
    This reverts commit 6c778a27fbeca0e32028d98ea2bb34c94353207e.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import Header from './components/Layout/Header';
-import { useState } from 'react';
+import { Fragment, useState } from 'react';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
-import CartProvider from './store/CartProvider';
 
 function App() {
   const [cartIsShown, setCartIsShown] = useState(false);
@@ -16,13 +15,13 @@ function App() {
   };
 
   return (
-    <CartProvider>
+    <Fragment>
       {cartIsShown && <Cart onClose={hideCartHandler}/>} {/** show cart modal only if cartIsShown is true */}
       <Header onShowCart={showCartHandler}></Header>
       <main>
         <Meals></Meals>
       </main>
-    </CartProvider>
+    </Fragment>
   );
 }
 
