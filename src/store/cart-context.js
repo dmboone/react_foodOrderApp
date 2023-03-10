6c778a27fbeca0e32028d98ea2bb34c94353207e
Destiny Boone <destaj.db@gmail.com>
2023-03-10 14:54:42 -0500

    adding a cart context

---
+++ src/store/cart-context.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+// the purpose if this file is really just to have better autocomplete
+React.createContext({
+    items: [],
+    totalAmount: 0,
+    addItem: (item) => {
+
+    },
+    removeItem: (id) => {
+
+    },
+});
+
+export default CartContext;
